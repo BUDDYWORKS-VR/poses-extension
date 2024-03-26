11ee39aa3b8169309b26f8fa802630e516aacf16
JustBuddy <8122264+JustBuddy@users.noreply.github.com>
2024-03-26 04:45:47 +0100

    2.0.1

--- Editor/AddToHierarchy.cs
+++ Editor/AddToHierarchy.cs
@@ -7,21 +7,35 @@ namespace BUDDYWORKS.PosesExtension.AddToHierarchy
     {
         public static string prefabGUID = "ff714a403f2fe944aa77358222a4be1c";
         public static string prefabGUID2 = "09cb0fd5fac430446b22b023b90bd66d";
+        public static string prefabGUID3 = "10b55c87769faa544ae55a6de658bf86";
+        public static string prefabGUID4 = "d1e19656881f0994b880e2ea7164e6bf";
 
         // Toolbar Menu
-        [MenuItem("BUDDYWORKS/Poses Extension/Spawn Prefab...")]
+        [MenuItem("BUDDYWORKS/Poses Extension/Spawn Prefab...", false, 0)]
         public static void SpawnPE()
         {
             SpawnPrefab(prefabGUID);
         }
 
-        [MenuItem("BUDDYWORKS/Poses Extension/Spawn Prefab... (GGL Experimental)")]
+        [MenuItem("BUDDYWORKS/Poses Extension/Spawn Prefab... (GGL Experimental)", false, 1)]
         public static void SpawnPEGGL()
         {
             SpawnPrefab(prefabGUID2);
         }
 
-        public static void SpawnPrefab(string guid)
+        [MenuItem("BUDDYWORKS/Poses Extension/Spawn GoGo Loco Beyond Prefab (If Imported)", false, 30)]
+        public static void SpawnGGLBeyond()
+        {
+            SpawnPrefab(prefabGUID3);
+        }
+
+        [MenuItem("BUDDYWORKS/Poses Extension/Spawn GoGo Loco All Prefab (If Imported)", false, 40)]
+        public static void SpawnGGLAll()
+        {
+            SpawnPrefab(prefabGUID4);
+        }
+
+        private static void SpawnPrefab(string guid)
         {
             string prefabPath = AssetDatabase.GUIDToAssetPath(guid);
 
